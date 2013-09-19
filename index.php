97eeb38bddd3d9dd7572d801102c6b0a82825436
Schnabulation <steve.heller@schnabulation.ch>
2013-09-19 16:22:25 +0200

    All finished - except crap sendmail not installed

--- index.php
+++ index.php
@@ -1,38 +1,21 @@
 <?php
-include 'header.php';
-?>
+include 'includes/settings.inc';
+include 'includes/header.inc';
+include 'includes/sendmail.inc';
 
-<?php
+$ZHAWSendMail = new ZHAWSendMail("sh@schnabulation.ch");
+
 DrawHeaderAndFooter::drawHeader();
-?>
+
+echo "
   <header>
   <h1>PHP und MySQL - eine Einführung</h1>
   </header>
-  <nav>
-  <ul>
-  <li><a href="#">Link zu Element 1</a></li>
-  <li><a href="#">Link zu Element 2</a></li> 
-  <li><a href="#">Link zu Element 3</a></li>
-  </ul>
-  </nav>
-    <article>Frankfurter sirloin pork turducken pork chop beef ribs meatball. 
-    Shank bacon flank pork chicken, ham pig ribeye hamburger tail tenderloin
-    ham hock capicola. Pork belly leberkas pork t-bone corned beef chuck ribeye
-    meatball capicola shoulder. Chicken brisket drumstick, boudin tongue
-    spare ribs frankfurter cow tenderloin salami pork belly pork chop swine. Bacon
-    shankle pork, chicken salami meatball ball tip pastrami tenderloin swine
-    doner jowl ham pork loin. Beef ribs spare ribs pork chop shoulder salami chicken.
-    Pig ham hock ribeye venison frankfurter.</article>
-    
-    <video width="320" height="240" controls><source src="video.mp4" type="video/mp4" /></video> 
-    
-    <article>Bresaola salami swine shankle strip steak meatball. Flank ball
-    tip fatback biltong ham hock chicken. Tri-tip ribeye turducken frankfurter meatloaf
-    tongue capicola pancetta turkey bresaola. Pork belly jerky cow swine beef ribs
-    short loin pancetta shankle andouille salami. Cow beef ribs sirloin, turducken
-    strip steak shankle ball tip ham hock doner flank tongue pork belly.</article>
-    
-    <button type="button" name="" value="" class="css3button">GO 4 IT</button>
-<?php 
+    <article>Sobald diese PHP-Seite lädt, wird mittels Klasse ZHAWSendMail ein Mail an
+	<a href=\"mailto:" . $ZHAWSendMail->getDestination() . "\">" . $ZHAWSendMail->getDestination() . "</a> geschickt.</article>
+";
+
+$ZHAWSendMail->sendMailToxobe();
+
 DrawHeaderAndFooter::drawFooter();
 ?>
