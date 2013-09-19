2b5646378df4c5b5887600b0baba16eb7db02ed8
Schnabulation <steve.heller@schnabulation.ch>
2013-09-19 11:57:27 +0200

    Added the stuff from the main repo here

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include 'header.php';
+?>
+
+<?php
+DrawHeaderAndFooter::drawHeader();
+?>
+  <header>
+  <h1>PHP und MySQL - eine Einführung</h1>
+  </header>
+  <nav>
+  <ul>
+  <li><a href="#">Link zu Element 1</a></li>
+  <li><a href="#">Link zu Element 2</a></li> 
+  <li><a href="#">Link zu Element 3</a></li>
+  </ul>
+  </nav>
+    <article>Frankfurter sirloin pork turducken pork chop beef ribs meatball. 
+    Shank bacon flank pork chicken, ham pig ribeye hamburger tail tenderloin
+    ham hock capicola. Pork belly leberkas pork t-bone corned beef chuck ribeye
+    meatball capicola shoulder. Chicken brisket drumstick, boudin tongue
+    spare ribs frankfurter cow tenderloin salami pork belly pork chop swine. Bacon
+    shankle pork, chicken salami meatball ball tip pastrami tenderloin swine
+    doner jowl ham pork loin. Beef ribs spare ribs pork chop shoulder salami chicken.
+    Pig ham hock ribeye venison frankfurter.</article>
+    
+    <video width="320" height="240" controls><source src="video.mp4" type="video/mp4" /></video> 
+    
+    <article>Bresaola salami swine shankle strip steak meatball. Flank ball
+    tip fatback biltong ham hock chicken. Tri-tip ribeye turducken frankfurter meatloaf
+    tongue capicola pancetta turkey bresaola. Pork belly jerky cow swine beef ribs
+    short loin pancetta shankle andouille salami. Cow beef ribs sirloin, turducken
+    strip steak shankle ball tip ham hock doner flank tongue pork belly.</article>
+    
+    <button type="button" name="" value="" class="css3button">GO 4 IT</button>
+<?php 
+DrawHeaderAndFooter::drawFooter();
+?>
---
+++ startup.sh
@@ -0,0 +1,19 @@
+sudo locale-gen de_CH.utf8
+sudo update-locale LANG=de_CH.utf8
+sudo ln -sf /usr/share/zoneinfo/Europe/Zurich /etc/localtime
+sudo apt-get update
+sudo apt-get install -y mc
+sudo apt-get install -y apache2-mpm-prefork php5 php5-curl php5-dev php5-gd php5-idn php5-imagick php5-imap php5-mysql php5-sqlite php5-xcache php5-mcrypt php5-xsl
+sudo a2enmod suexec rewrite ssl actions include vhost_alias
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password virtual'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password virtual'
+sudo apt-get install -y mysql-server-5.5
+sudo rm -rf /var/www
+sudo ln -fs /vagrant /var/www
+echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/app-password-confirm password virtual' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/admin-pass password virtual' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/mysql/app-pass password virtual' | debconf-set-selections
+echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections
+sudo apt-get install -q -y phpmyadmin
+sudo service apache2 reload
