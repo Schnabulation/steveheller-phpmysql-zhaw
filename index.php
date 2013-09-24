69ad1d1ed5db4ed7e481e1c2c15734c1631e8e3e
Schnabulation <steve.heller@schnabulation.ch>
2013-09-24 18:19:02 +0200

    Reconfigured sendmail-class to use PHPMailer

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include 'includes/settings.inc';
 include 'includes/header.inc';
 include 'includes/sendmail.inc';
+include 'includes/passwords.inc';
 
 $ZHAWSendMail = new ZHAWSendMail("sh@schnabulation.ch");
 
@@ -12,10 +13,10 @@ echo "
   <h1>PHP und MySQL - eine Einführung</h1>
   </header>
     <article>Sobald diese PHP-Seite lädt, wird mittels Klasse ZHAWSendMail ein Mail an
-	<a href=\"mailto:" . $ZHAWSendMail->getDestination() . "\">" . $ZHAWSendMail->getDestination() . "</a> geschickt.</article>
+	<a href=\"mailto:sh@schnabulation.ch\">sh@schnabulation.ch</a> geschickt.</article>
 ";
 
-$ZHAWSendMail->sendMailToxobe();
+$ZHAWSendMail->sendMailTo("This is the subject", "I want to tell you something!");
 
 DrawHeaderAndFooter::drawFooter();
 ?>
